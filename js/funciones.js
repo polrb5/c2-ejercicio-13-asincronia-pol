7b5726b72e175e00f22594aac297fa57d245162a
Pol Reig <polreigbroto@gmail.com>
2021-06-07 13:40:37 +0200

    Elementos DOM asignados y función vaciar personajes añadida

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import { personajes } from "./personajes.js";
 
 export const getPersonajes = () =>
--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+/* eslint-disable import/extensions */
+import { getPersonajes, mataPersonajes } from "./funciones.js";
 
+// Elementos DOM
+const personajesElemento = document.querySelector(".personajes");
+const personajeDummy = document.querySelector(".personaje.dummy");
+const botonCargarPersonajes = document.querySelector(".cargar-personajes");
+const botonMatarFamilia = document.querySelector(".matar-familia");
+
+const vaciarPersonajes = () => {
+  for (const personjesElemento of personajesElemento.querySelector("personaje"))
+    personajesElemento.remove();
+};
