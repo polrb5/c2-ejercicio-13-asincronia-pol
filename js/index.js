5b26a25d0d496ca88688fbdf3c0c3677fa16dcdd
Pol Reig <polreigbroto@gmail.com>
2021-06-07 13:55:32 +0200

    Pintar personajes con sus datos

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ const vaciarPersonajes = () => {
   for (const personjesElemento of personajesElemento.querySelector("personaje"))
     personajesElemento.remove();
 };
+
+const pintarPersonajes = (personajes) => {
+  vaciarPersonajes();
+  for (const personaje of personajes) {
+    const { nombre, familia, estado } = personaje;
+    const personajeMolde = document
+      .querySelector(".personaje-dummy")
+      .cloneNode(true);
+    personajeMolde.classList.remove("personaje-dummy");
+    personajeMolde.querySelector(".nombre").textContent = nombre;
+    personajeMolde.querySelector(".familia").textContent = familia;
+    personajeMolde.querySelector(".estado").textContent = estado
+      ? "vivo"
+      : "muerto";
+  }
+};
